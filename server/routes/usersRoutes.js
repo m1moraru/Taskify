d952c1f3d03cdd21efb213046c3563f1a430d09e
m1moraru <164013436+m1moraru@users.noreply.github.com>
2025-01-09 23:34:10 +0000

    Update usersRoutes.js

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const usersController = require('../controllers/usersController');
-const ensureAuthenticated = require('../middleware/ensureAuthenticated'); // Ensure middleware is correctly imported
+const ensureAuthenticated = require('../middleware/ensureAuthenticated'); 
 const router = express.Router();
 
 router.post('/register', usersController.createUser); 
 router.post('/login', usersController.loginUser); 
 
 router.get('/me', ensureAuthenticated, usersController.getMe);
-router.get('/created_at', ensureAuthenticated, usersController.getUserCreatedAt); // Specific routes first
-router.get('/:id', ensureAuthenticated, usersController.getUser); // Dynamic route last
+router.get('/created_at', ensureAuthenticated, usersController.getUserCreatedAt); 
+router.get('/:id', ensureAuthenticated, usersController.getUser); 
 router.put('/:id', ensureAuthenticated, usersController.updateUser);
 router.delete('/:id', ensureAuthenticated, usersController.deleteUser);
 
