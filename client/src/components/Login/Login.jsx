cda0fefc41b843c843da8a65e1435860fbe88325
m1moraru <164013436+m1moraru@users.noreply.github.com>
2025-01-10 01:06:22 +0000

    Update Login.jsx

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import '../Login/Login.css';
 
-const API_BASE_URL = 'https://taskify-nuog.onrender.com/api'; // Base URL for the API
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '/api';
 
 const Login = () => {
   const { login } = useContext(AuthContext);
