d1df1641471432eaaf78e36f251618cd9575fc60
m1moraru <164013436+m1moraru@users.noreply.github.com>
2025-01-11 12:08:30 +0000

    Update Homepage.jsx

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -10,6 +10,7 @@ import { AuthContext } from '../context/AuthContext';
 import MobileNav from '../components/MobileNav/MobileNav';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '/api'; 
+console.log('API_BASE_URL:', API_BASE_URL);
 
 function Homepage() {
   const { user, logout } = useContext(AuthContext);
