dc620a9372d98e6b6cd7cbb9f62fbfe6878969d3
m1moraru <164013436+m1moraru@users.noreply.github.com>
2025-02-03 23:38:32 +0000

    Update TaskList.jsx

--- client/src/components/TaskList/TaskList.jsx
+++ client/src/components/TaskList/TaskList.jsx
@@ -18,8 +18,6 @@ function TaskList() {
     fetchTasks();
   }, []);
 
-  const API_BASE_URL = (process.env.REACT_APP_API_BASE_URL || "https://taskify-nuog.onrender.com/api").replace(/\/$/, "");
-
   const fetchTasks = async () => {
     try {
       const url = `${API_BASE_URL}/tasks`;
